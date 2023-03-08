e4bc678a9e0ef0ac309843d28f264eea2b3bab2f
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-03-08 09:23:08 -0800

    INSERT and UPDATE using RECORD

---
+++ plsql/3-dml-using-records.sql
@@ -0,0 +1,32 @@
+-- 1. INSERT
+DECLARE
+    v_rec customer%ROWTYPE;
+BEGIN
+    SELECT *
+    INTO v_rec
+    FROM customer
+    WHERE customer_id = 6 ;
+    
+    v_rec.customer_id := 999 ;    -- changing CUSTOMER_ID in the record
+    
+    INSERT INTO customer
+    VALUES v_rec ;    -- RECORD containing a whole row is being used as value
+END ;
+
+
+-- 2. UPDATE
+DECLARE
+    v_rec customer%ROWTYPE ;
+BEGIN
+    SELECT *
+    INTO v_rec
+    FROM customer
+    WHERE customer_id = 1 ;
+    
+    v_rec.deposit := 777 ;
+    
+    UPDATE customer
+    SET ROW = v_rec  -- Record as new values
+    WHERE customer_id = 1 ;
+    
+END ;
