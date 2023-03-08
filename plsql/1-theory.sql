6bf9cef44d163e35f5607b3beb5201daee9acf5b
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-03-08 15:01:14 -0800

    Theory

---
+++ plsql/1-theory.sql
@@ -0,0 +1,9 @@
+/* 
+1. Three main categories of oracle data types are
+scalar datatype: number, pls_integer, char, varchar2, boolean, clob, date etc. (hold 1 value in 1 variable)
+composite datatype: records, collections (hold more than 1 values in 1 variable)
+reference datatype: ref cursors
+
+
+2. Collection is an ORDERED group of LOGICALLY RELATED elements (Array in programming)
+*/
