13b469c71d93a8907505fa84c1a603290e580caf
Artyom Pak <44568508+artyomvp1@users.noreply.github.com>
2023-03-14 18:02:41 -0700

    Bulk binding concepts

--- plsql/10-bulk-binding.sql
+++ plsql/10-bulk-binding.sql
@@ -1,5 +1,5 @@
 /*
-How Oracle executes PL/SQL code
+-- 1. How Oracle executes PL/SQL code
 
 Two engines that executes code:
 1 - PL/SQL engine - all the procedural code (Loops, if-else, etc...)
@@ -17,6 +17,25 @@ Types of binding:
 3) DEFINE - when SELECT or FETCH statements assign a DB value to a PL/SQL or host variable
 
 To implement BULK BINDING we use:
-1) FORALL (in-bind and out-bind)
-2) BULK COLLECT (for DEFINE binding)
+1) In-binding - transfering information from a collection into a table using FORALL
+2) Out-binding - After DML operation saving modified data into a collection (Result of a DML statement into a collection) with minimal context switches. RETURNING BULK COLLECT INTO
+3) DEFINE - When we want to hold the result of SQL statement in collection variable using single context switch (SELECT BULK COLLECT INTO)
 */
+
+--2. FETCH Using bulk collect
+DECLARE
+    TYPE c_type IS TABLE OF NUMBER INDEX BY PLS_INTEGER ;
+    v_collection c_type ;
+    CURSOR v_cur IS
+        SELECT customer_id
+        FROM customer ;
+BEGIN
+    OPEN v_cur ;
+    FETCH v_cur BULK COLLECT INTO v_collection ; -- BULK COLLECT FETCHING
+    CLOSE v_cur ;
+    
+    FOR i IN v_collection.FIRST .. v_collection.LAST 
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(v_collection(i)) ;
+    END LOOP ;
+END ;
